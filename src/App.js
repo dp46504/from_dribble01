3e7214350647903b27b2aaa09f1736390cac763e
Pawel Durczak <kissonion24@gmail.com>
2022-04-09 01:23:25 +0200

    App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { GlobalStyle } from "./Style";
+import HeaderComponent from "./components/HeaderComponent";
+import BodyComponent from "./components/BodyComponent";
 function App() {
-  return <></>;
+  return (
+    <>
+      <GlobalStyle></GlobalStyle>
+      <HeaderComponent></HeaderComponent>
+      <BodyComponent></BodyComponent>
+    </>
+  );
 }
 
 export default App;
